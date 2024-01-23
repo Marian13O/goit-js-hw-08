b1505c8c44700591f6523fecc006a01666c78b71
Marian13O <142900026+Marian13O@users.noreply.github.com>
2024-01-23 18:45:18 +0200

    edit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import _throttle from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
